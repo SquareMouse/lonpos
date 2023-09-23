6f7853fdb8e5b3cb08786cc50563e2b0bd89dce2
Chandler Chen <cgchen@berkeley.edu>
2023-09-23 14:30:19 -0700

    it's messy but this should find them all

--- game_state.py
+++ game_state.py
@@ -3,9 +3,12 @@ from dataclasses import dataclass, field
 from typing import List, Tuple, Dict
 import pieces
 import scipy
-import copy
+# import copy
 from pprint import pprint
 from termcolor import cprint
+import sys
+import time
+import multiprocessing
 
 @dataclass(slots=True)
 class PiecePlacement:
@@ -33,6 +36,17 @@ class PiecePlacement:
             return False
         return True
     
+    def copy(self) -> 'PiecePlacement':
+        # return copy.copy(self)
+        return PiecePlacement(
+            piece = self.piece,
+            rotation = self.rotation,
+            flipped = self.flipped,
+            row = self.row,
+            col = self.col,
+            is_placed = self.is_placed
+        )
+    
 
 BOARD_SIZE = 10
 def make_board():
@@ -55,17 +69,14 @@ class GameState:
         '''only choose ones that are touching an edge'''
         possible_placements = []
         if depth is not None:
-            unplaced = [copy.deepcopy(self.placements[depth])]
+            unplaced = [self.placements[depth].copy()]
         else:
-            unplaced = [copy.deepcopy(p) for p in self.placements if not p.is_placed]
+            unplaced = [p for p in self.placements if not p.is_placed]
         for placement in unplaced:
             placed = False
             for flip in placement.piece.available_flip:
                 for rot in placement.piece.available_rotations:
-                    placement1 = PiecePlacement(
-                        piece=placement.piece   
-                    )
-                    copy.deepcopy(placement)
+                    placement1 = placement.copy()
                     placement1.flipped = flip
                     placement1.rotation = rot
                     shape = np.rot90(placement1.oriented_shape(), 2)  # for the convolution
@@ -91,10 +102,10 @@ class GameState:
     
     def apply_placement(self, placement: PiecePlacement) -> 'GameState':
         placement_index = _color_to_placement_index[placement.piece.color]
-        newGameState = copy.deepcopy(self)
+        newGameState = self.copy()
         # newGameState.board = self.board
         assert placement.is_placed
-        newGameState.placements[placement_index] = copy.copy(placement)
+        newGameState.placements[placement_index] = placement.copy()
         shape = placement.oriented_shape()
         num_shape_rows, num_shape_cols = shape.shape
         for row in range(num_shape_rows):
@@ -119,6 +130,10 @@ class GameState:
                 cprint("  ", "black", pieces.Color(cell).termcolor(), end="")
             print()
 
+    def copy(self) -> 'GameState':
+        return GameState(board=self.board.copy(),
+                                 placements=[p.copy() for p in self.placements])
+
 def apply_all_placements(placements, current_board: np.ndarray) -> np.ndarray:
     board = current_board.copy()
 
@@ -132,24 +147,26 @@ def apply_all_placements(placements, current_board: np.ndarray) -> np.ndarray:
                 board[row + placement.row, col + placement.col] |= shape[row, col]
     return board
 
+
+
+
 @dataclass
 class GameTree:
     successful_placements: List[List[PiecePlacement]] = field(default_factory=list)
 
-    def find_all_placements(self, game_state = GameState(), depth=0):
+    def find_all_placements(self, game_state = GameState(), depth=0, start_time=None):
         if all([p.is_placed for p in game_state.placements]):
             self.successful_placements.append(game_state.placements)
-            print(f"found {len(self.successful_placements)} solutions at {time.time() - start_time}")
+            if start_time is not None:
+                print(f"found {len(self.successful_placements)} solutions at {time.time() - start_time}")
+            else:
+                print("None start time")
             game_state.show()
+            # if len(self.successful_placements) == 2:
+            #     sys.exit()
             return
         
-        # if depth <= 5:
-        #     print([p.is_placed for p in game_state.placements])
-        #     print(game_state.board)
-        
-        # all_placements = game_state.find_placements()
 
-        # # if len(all_placements) < 900:
         all_applied_board = apply_all_placements(game_state.find_placements(), current_board=game_state.board)
         if np.min(all_applied_board) == 0:
             # print(f"Impossible at depth {depth} with len placements {len(all_placements)}")
@@ -162,36 +179,49 @@ class GameTree:
             #     print(f"{' | ' * depth} {idx} / {len(next_piece_placements)} {time.time() - start_time}")
                 
             new_game_state = game_state.apply_placement(placement)
-            self.find_all_placements(new_game_state, depth=depth + 1)
+            self.find_all_placements(new_game_state, depth=depth + 1, start_time=start_time)
 
-        
+def solve_game_tree(placement: PiecePlacement, depth: int, initial_game_state: GameState, start_time) -> GameTree:
 
-        
+    gt = GameTree()
+    gt.find_all_placements(initial_game_state.apply_placement(placement), depth=depth, start_time = start_time)
+    return gt
+
+if __name__ == '__main__':
     
-import time
-start_time = time.time()
-gt = GameTree()
+    start_time = time.time()
 
-initial_game_state = GameState()
-initial_game_state.placements[0].row = 8
-initial_game_state.placements[0].col = 6
-initial_game_state.placements[0].is_placed = True
 
-initial_game_state.placements[1].row = 9
-initial_game_state.placements[1].col = 1
-initial_game_state.placements[1].flipped = True
-initial_game_state.placements[1].rotation = 2
-initial_game_state.placements[1].is_placed = False
+    initial_game_state = GameState()
+    initial_game_state.placements[0].row = 8
+    initial_game_state.placements[0].col = 6
+    initial_game_state.placements[0].is_placed = True
 
-initial_game_state.board = apply_all_placements(initial_game_state.placements, initial_game_state.board)
-gt.find_all_placements(initial_game_state, depth=1)
+    initial_game_state.placements[1].row = 9
+    initial_game_state.placements[1].col = 1
+    initial_game_state.placements[1].flipped = True
+    initial_game_state.placements[1].rotation = 2
+    initial_game_state.placements[1].is_placed = True
 
-start_time = time.time()
-print(f"terminated after {time.time() - start_time} seconds")
-for placement in gt.successful_placements:
-    print(placement)
-# g = apply_all_placements(gt.successful_placements[0])
-# print(g.board)
+    initial_game_state.board = apply_all_placements(initial_game_state.placements, initial_game_state.board)
+
+    starting_placements = initial_game_state.find_placements(depth=2)
+
+    start_time = time.time()
+
+    with multiprocessing.Pool() as p:
+        args = [(sp, 3, initial_game_state, start_time) for sp in starting_placements]
+        solved_game_trees = p.starmap(solve_game_tree, args)
+        
+    # gt.find_all_placements(initial_game_state, depth=2)
+
+
+
+    print(f"terminated after {time.time() - start_time} seconds")
+    for solved in solved_game_trees:
+        for placement in solved.successful_placements:
+            print(placement)
+
 
         
         
